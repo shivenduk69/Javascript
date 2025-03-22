a95bea809bb1bbaf618793babe734c8e0754d05b
shivendu <shivenduk69@gmail.com>
2025-03-22 09:24:02 +0530

    Added array_methods

---
+++ 3_Arrays/arrays_methods.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, true, "Hello"];
+//push()
+arr.push("manish");
+console.log(arr);
+//pop()
+arr.pop();
+console.log(arr);
+//shift
+arr.shift("manish");
+console.log(arr);
+//unshift
+arr.unshift();
+console.log(arr);
+//sort
+let brr = ["Shivendu", "Aman", "priyanshu"];
+brr.sort();
+console.log(brr);
+//reverse
+arr.reverse();
+console.log(arr);
